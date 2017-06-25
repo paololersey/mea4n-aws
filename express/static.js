e7405b7c841820fb6cc84861a627ee83878aed99
paolokersey <paolo_spadoni@yahoo.it>
2017-06-25 16:43:14 +0200

    frontend

---
+++ express/static.js
@@ -0,0 +1,44 @@
+var express = require('express')
+var router = require('express').Router()
+var sysInfo  = require('../utils/sys-info')
+
+router.use(express.static(__dirname + '/../frontend/dist'))
+router.get('/', function (req, res) {
+  res.sendfile('frontend/dist/index.html')
+})
+
+
+router.get('/api/messages', function (req, res, next) {
+    Post.find()
+        .sort('-date')
+        .exec((err, messages) => {
+            if (err) {
+                return next(err)
+            }
+            res.json(messages)
+        })
+})
+
+
+// openshift monitoring services
+
+router.get('/health', (req, res) => {
+  res.writeHead(201);
+  res.end();
+})
+
+router.get('/info/gen', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 'no-cache, no-store');
+  console.log(sysInfo[req.url.slice(6)]());
+  res.end(JSON.stringify(sysInfo[req.url.slice(6)]()));
+})
+
+router.get('/info/poll', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 'no-cache, no-store');
+  res.end(JSON.stringify(sysInfo[req.url.slice(6)]()));
+})
+
+
+module.exports = router
