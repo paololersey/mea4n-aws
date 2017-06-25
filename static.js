932e08f632246ec0d9425834da6abf1ca0b2c4f0
paolokersey <paolo_spadoni@yahoo.it>
2017-06-25 16:55:31 +0200

    static

--- static.js
+++ static.js
@@ -3,10 +3,10 @@ var router = require('express').Router()
 var sysInfo  = require('../utils/sys-info')
 
 //router.use(express.static(__dirname + '/../frontend/dist'))
-router.use(express.static(__dirname + '/../public'))
+router.use(express.static(__dirname + '/public'))
 router.get('/', function (req, res) {
   //res.sendfile('frontend/dist/index.html')
-  res.sendfile('public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+  res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 })
 
 
