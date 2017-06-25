a7d1ffff311882e01673209c430d46bb1fc15d83
paolokersey <paolo_spadoni@yahoo.it>
2017-06-25 16:24:48 +0200

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	// the database url to connect!
+	//url: 'mongodb://todoAholic:scotchio@ec2-52-0-14-185.compute-1.amazonaws.com:27017/dummyDB'
+	url: 'mongodb://paolokerseydummy:dummyDB@ec2-34-252-176-130.eu-west-1.compute.amazonaws.com:27017/dummyDB'
+}
