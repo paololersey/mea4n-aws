070739aca2270dd93f92f7af951b7d19f72852b7
paolokersey <paolo_spadoni@yahoo.it>
2017-06-30 22:29:41 +0200

    db

--- db.js
+++ db.js
@@ -11,9 +11,9 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   process.env.OPENSHIFT_APP_NAME;
 }
 
-if(process.env.LOGNAME){
-    connectionString = 'mongodb://paolo.spadoni:kersey8D1@http://ec2-52-50-66-116.eu-west-1.compute.amazonaws.com:27017/machine';
-}
+//if(process.env.LOGNAME){
+connectionString = 'mongodb://paolo.spadoni:kersey8D1@http://ec2-52-50-66-116.eu-west-1.compute.amazonaws.com:27017/machine';
+//}
 
 mongoose.connect(connectionString, function () {
     console.log('mongodb connected')
